bdd8412f9394d1e0c42fa5a3baa6548a1cbd0cc2
ArivuAlamari <arivu.alamari@gmail.com>
2021-01-05 18:12:26 +0100

    fix compile error

--- libbpf-tools/bpflist.c
+++ libbpf-tools/bpflist.c
@@ -1,13 +1,24 @@
-include <argp.h>
+#include <argp.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <time.h>
 #include <unistd.h>
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 #include "bpflist.h"
 #include "bpflist.skel.h"
 #include "trace_helpers.h"
+
+
+const char *argp_program_version = "bpflist 0.1";
+const char *argp_program_bug_address = "<bpf@vger.kernel.org>";
+const char argp_program_doc[] =
+"Display processes currently using BPF programs and maps. Ctrl-C to end.\n"
+"\n"
+"USAGE: cpufreq [--help] [-v Verbose]\n"
+"\n";
+
+static const struct argp_option opts[] = {
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{},
+};
+
 
